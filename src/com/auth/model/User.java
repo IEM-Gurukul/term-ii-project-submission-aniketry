1745879c3d09df807aa055f80c8e041e22e7f1f3
Aniket Roy <aniketroy0406@gmail.com>
2026-03-29 17:39:52 +0530

    I have improved user authentication

--- src/com/auth/model/User.java
+++ src/com/auth/model/User.java
@@ -13,7 +13,27 @@ public class User {
         return username;
     }
 
+public void setUsername(String username) {
+        if (username == null || username.isEmpty()) {
+            System.out.println("Username cannot be empty");
+            return;
+        }
+        this.username = username;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        if (password.length() < 6) {
+            System.out.println("Password must be at least 6 characters");
+            return;
+        }
+        this.password = password;
+    }
+
+    public String toString() {
+        return username;
     }
 }
