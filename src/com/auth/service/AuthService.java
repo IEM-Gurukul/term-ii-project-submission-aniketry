d1efe16cba83dccdc710a3800c8f2967941ab588
Aniket Roy <aniketroy0406@gmail.com>
2026-03-29 17:45:28 +0530

    Removed the unnecessary  exception

--- src/com/auth/service/AuthService.java
+++ src/com/auth/service/AuthService.java
@@ -2,9 +2,10 @@ package com.auth.service;
 
 import com.auth.model.User;
 
-public interface AuthService {
+public interface AuthService 
+{
 
-    void register(User user) throws Exception;
+    void register(User user);
 
-    boolean login(String username, String password) throws Exception;
+    boolean login(String username, String password);
 }
